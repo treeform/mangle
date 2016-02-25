1c76c45055701dfcc9ccad5cdf6e321b1d85f2f0
baabelfish <baabelfish@gmail.com>
2016-02-25 08:17:50 +0200

    Change language

--- example.nim
+++ example.nim
@@ -41,7 +41,7 @@ proc getIcehockeyTeams(): auto =
 # Make a lazy generator of team names
 var idGenerator = infinity()
     .drop(1)
-    .mapIt("Tiimit " & $it)
+    .mapIt("Teams " & $it)
 
 # Combine football and hockey
 zip(getFootballTeams(), getIcehockeyTeams())
@@ -52,8 +52,8 @@ zip(getFootballTeams(), getIcehockeyTeams())
     .eachIt(it.echo)
 
 # Echoes:
-# (id: Tiimit 1, football: (id: "FCB", name: "Bayern"), hockey: (id: ANA, name: Anaheim Ducks, seasons: 20112012, 20122013, 20132014, 20142015))          
-# (id: Tiimit 2, football: (id: "WOB", name: "Wolfsburg"), hockey: (id: ARI, name: Arizona Coyotes, seasons: 20112012, 20122013, 20132014, 20142015))     
-# (id: Tiimit 3, football: (id: "SGE", name: "Eintr. Frankfurt"), hockey: (id: BOS, name: Boston Bruins, seasons: 20112012, 20122013, 20132014, 20142015))
+# (id: Teams 1, football: (id: "FCB", name: "Bayern"), hockey: (id: ANA, name: Anaheim Ducks, seasons: 20112012, 20122013, 20132014, 20142015))          
+# (id: Teams 2, football: (id: "WOB", name: "Wolfsburg"), hockey: (id: ARI, name: Arizona Coyotes, seasons: 20112012, 20122013, 20132014, 20142015))     
+# (id: Teams 3, football: (id: "SGE", name: "Eintr. Frankfurt"), hockey: (id: BOS, name: Boston Bruins, seasons: 20112012, 20122013, 20132014, 20142015))
 # ...
 
