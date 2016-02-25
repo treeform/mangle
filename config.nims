b95b1944610c7ea40a3a5bde0d6121bed33ff5b4
baabelfish <baabelfish@gmail.com>
2016-02-25 08:12:00 +0200

    Generate doc, add test for example

--- config.nims
+++ config.nims
@@ -1,15 +1,12 @@
 task docs, "":
     exec "nim doc --threads:on mangle.nim"
-    setCommand "nop"
 
 task build, "":
     exec "nim c --threads:on -d:release mangle.nim"
-    setCommand "nop"
 
 task tests, "":
     exec "nim c --threads:on -d:releae -r mangle_test.nim"
-    setCommand "nop"
+    exec "nim c --threads:on -d:releae -r example.nim"
 
 task bench, "":
     exec "nim c --threads:on -d:release -r mangle_bench.nim"
-    setCommand "nop"
--- example.nim
+++ example.nim
@@ -10,7 +10,7 @@ proc getTeamData(api: string): auto =
 
 
 proc getFootballTeams(): auto =
-    proc get(node: JsonNode, key: string): JsonNode = node[key]
+    proc get(node: JsonNode, key: string): JsonNode = node[key] # This should really be in the json module
     getTeamData("http://api.football-data.org/v1/soccerseasons/351/teams")
         .get("teams")
         .getElems
@@ -38,8 +38,12 @@ proc getIcehockeyTeams(): auto =
                 .join(", ")))
 
 
-var idGenerator = infinity().drop(1).mapIt("Tiimit " & $it)
+# Make a lazy generator of team names
+var idGenerator = infinity()
+    .drop(1)
+    .mapIt("Tiimit " & $it)
 
+# Combine football and hockey
 zip(getFootballTeams(), getIcehockeyTeams())
     .mapIt((
         id: idGenerator.invoke,
@@ -52,3 +56,4 @@ zip(getFootballTeams(), getIcehockeyTeams())
 # (id: Tiimit 2, football: (id: "WOB", name: "Wolfsburg"), hockey: (id: ARI, name: Arizona Coyotes, seasons: 20112012, 20122013, 20132014, 20142015))     
 # (id: Tiimit 3, football: (id: "SGE", name: "Eintr. Frankfurt"), hockey: (id: BOS, name: Boston Bruins, seasons: 20112012, 20122013, 20132014, 20142015))
 # ...
+
